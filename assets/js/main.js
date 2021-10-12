893c7b7933347db8f821dc8592c0170e2725d515
tiny999 <olawalesalami3@gmail.com>
2021-10-12 21:31:56 +0100

    faqs page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,23 @@
+ 
+  $(document).ready(function(){
+    $('#accordion .head').click(function(){
+        $(this).toggleClass('active');
+        $(this).parent().find('.arrow').toggleClass('arrow-animate');
+        $(this).parent().find('.content').slideToggle(280);
+
+        if ($('#accordion .head.active').length > 1) {
+            $('#accordion .head').not(this).removeClass('active')
+            $('#accordion .head').not(this).parent().find('.arrow').removeClass('arrow-animate');
+            $('#accordion .head').not(this).parent().find('.content').slideUp(280);
+        }
+        
+    });
+  });
+
 document.addEventListener( 'DOMContentLoaded', function () {
     new Splide( '.splide', {
         arrows: false,
         autoplay: true
     } ).mount();
 } );
+
